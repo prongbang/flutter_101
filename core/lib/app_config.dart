a5c67a0a1e022ebf1dbb8e79feff3f67860b3cc6
prongbang <prongbang@gmail.com>
2021-04-19 17:36:30 +0700

    add flavor enum and extension

--- core/lib/app_config.dart
+++ core/lib/app_config.dart
@@ -13,3 +13,25 @@ class AppConfig {
   static String androidApplicationId = "com.krungsri.flutter101";
   static String iOSApplicationId = "flutter-101/id1234567890";
 }
+
+/// Used:
+/// String display = Flavor.production.display;
+///
+enum Flavor { production, uat, develop, mock }
+
+extension FlavorExtension on Flavor {
+  String get display {
+    switch (this) {
+      case Flavor.production:
+        return 'Production';
+      case Flavor.uat:
+        return 'UAT';
+      case Flavor.develop:
+        return 'Develop';
+      case Flavor.mock:
+        return 'Mock';
+      default:
+        return 'other';
+    }
+  }
+}
