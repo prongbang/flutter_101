130df47b72fa17a2d62790a577b44b88c14e3017
prongbang <prongbang@gmail.com>
2021-04-28 11:45:59 +0700

    add ios date picker

--- lib/src/widgets/_29date_picker/date_picker_page.dart
+++ lib/src/widgets/_29date_picker/date_picker_page.dart
@@ -1,14 +1,28 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DatePickerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TextButton(
-        onPressed: () {
-          _showDatePicker(context);
-        },
-        child: Text('Show Date Picker'),
+      child: Column(
+        children: [
+          TextButton(
+            onPressed: () {
+              _showDatePicker(context);
+            },
+            child: Text('Show Date Picker'),
+          ),
+          Container(
+            height: 400,
+            child: CupertinoDatePicker(
+              initialDateTime: DateTime.now(),
+              onDateTimeChanged: (val) {
+                print(val);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
