b42f7898973f9f539bc019cd5bf677dd93840499
prongbang <prongbang@gmail.com>
2021-04-28 11:49:09 +0700

    convert to stateful

--- lib/src/widgets/_28switch/switch_page.dart
+++ lib/src/widgets/_28switch/switch_page.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SwitchPage extends StatelessWidget {
+class SwitchPage extends StatefulWidget {
+  @override
+  _SwitchPageState createState() => _SwitchPageState();
+}
+
+class _SwitchPageState extends State<SwitchPage> {
+  bool _lights = false;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -9,16 +16,24 @@ class SwitchPage extends StatelessWidget {
           Text('SwitchListTile'),
           SwitchListTile(
             title: const Text('Lights'),
+            value: _lights,
+            onChanged: (bool value) {
+              setState(() {
+                _lights = value;
+              });
+            },
+            secondary: const Icon(Icons.lightbulb_outline),
+          ),
+          Text('Switch'),
+          Switch(
             value: true,
             onChanged: (bool value) {
               // setState(() {
               //   _lights = value;
               // });
             },
-            secondary: const Icon(Icons.lightbulb_outline),
           ),
-          Text('Switch'),
-          Switch(
+          CupertinoSwitch(
             value: true,
             onChanged: (bool value) {
               // setState(() {
