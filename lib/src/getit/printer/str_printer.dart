b2d664f50411f6de014587a9c25ecec5bee272ff
prongbang <prongbang@gmail.com>
2021-04-28 11:48:20 +0700

    rename function

--- lib/src/getit/printer/str_printer.dart
+++ lib/src/getit/printer/str_printer.dart
@@ -2,7 +2,7 @@ import 'package:flutter_101/src/getit/printer/printer.dart';
 
 class StrPrinter implements Printer {
   @override
-  void print(String message) {
+  void println(String message) {
     print(message);
   }
 }
